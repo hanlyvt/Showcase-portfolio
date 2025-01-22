1e419e7d74f46aed491ae958e51d63451a7f44da
hanlyvt <hanlytrang@gmail.com>
2025-01-22 10:59:02 +0100

    Upendo project + Indi Janssen project

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -9,36 +9,48 @@ const projects = [
     id: 1,
     title: "Upendo",
     coverImage: "/images/Upendo_Logo.svg?height=600&width=1200",
-    description: "Dit is een korte beschrijving van Project 1.",
-    date: "Januari 2023",
-    tools: ["Photoshop", "Illustrator", "InDesign"],
+    description:
+      "Upendo is een nieuwe tak die 2manydots graag wil ontwikkelen naast het bedenken en maken van websites. Naast het bouwen van websites willen ze ook focussen op het analyseren van deze websites. Daarom creëren ze Upendo, een onderdeel van 2md dat verschillende services biedt op het gebied van data-analyse voor websites. Upendo is een bedrijf dat simpelheid wil overbrengen naar de klant. Het doel van Upendo is dat klanten, na het bestellen en betalen voor het maken van een website, ook kiezen om gebruik te maken van Upendo.",
+    date: "Januari 2024",
+    tools: ["Figma", "Vscode"],
     reflection:
-      "Dit project was een uitdaging, maar ik heb er veel van geleerd...",
+      "Dit project heeft me inzicht gegeven in hoe belangrijk een goede structuur is bij zowel design als development. Door de Double Diamond Methode toe te passen, heb ik geleerd om vanuit een breed perspectief te starten, de juiste elementen te definiëren, en vervolgens een gefocust en effectief ontwerp te ontwikkelen. Ondanks mijn beperkte ervaring met Storyblok en React, kon ik met elke sprint stappen maken en mezelf verder ontwikkele",
     images: [
-      "/placeholder.svg?height=400&width=600",
-      "/placeholder.svg?height=400&width=600",
-      "/placeholder.svg?height=400&width=600",
+      "/images/Upendo_Laptop_2.svg?height=400&width=600",
+      "/images/Upendo_Laptop_3.svg?height=400&width=600",
+      "/images/Upendo_telefoon.svg?height=400&width=600",
     ],
-    additionalText:
-      "Hier is wat extra informatie over het project en het proces...",
+    additionalText: [
+      "Het Upendo-project bood een uitdagende en leerzame ervaring waarin ik zowel mijn design- als developmentvaardigheden heb kunnen ontwikkelen. Het proces was verdeeld in drie sprints, elk met specifieke doelen en mijlpalen.",
+      "Tijdens de eerste sprint richtte ik me op het begrijpen van de opdracht en de behoeften van de klant. Ik analyseerde verschillende data-analysewebsites, zoals ilionx, om inzicht te krijgen in hoe eenvoud en helderheid in complexe omgevingen kunnen worden toegepast. Vervolgens begon ik met het ontwerpen van een eerste mobiel design, gebaseerd op de brandguide van Upendo.",
+      "In de tweede sprint lag de focus op het verfijnen en finaliseren van het design. Ik verwerkte de feedback en onderzocht vergelijkbare websites zoals Ventriloc om inspiratie op te doen voor een heldere en gebruiksvriendelijke inhoudsverdeling.",
+      "De derde sprint richtte zich volledig op development. Mijn verantwoordelijkheid was de ontwikkeling van de analysis-pagina, een van de vijf servicepagina’s. Ik werkte aan de integratie tussen Storyblok en React en ontwikkelde herbruikbare componenten die op meerdere plekken konden worden toegepast.",
+    ],
   },
 
   {
     id: 2,
     title: "Indi Janssen portfolio",
     coverImage: "/images/Indi_Janssen_Logo.svg?height=600&width=1200",
-    description: "Dit is een korte beschrijving van Project 1.",
-    date: "Januari 2023",
-    tools: ["Photoshop", "Illustrator", "InDesign"],
+    description:
+      "Dit project richt zich op de vraag in hoeverre het creatieve proces van website design en ontwikkeling afhankelijk is van menselijke input, of dat AI-technologieën deze taken grotendeels kunnen overnemen. De hypothese is dat menselijke ideeën cruciaal blijven bij conceptontwikkeling en klantgerichte oplossingen. Dit wordt getest door een praktijkexperiment waarin AI-tools worden ingezet in het design- en ontwikkelingsproces.",
+    date: "December 2024",
+    tools: ["Figma", "V0.dev", "ChatGPT", "React", "TailwindCSS", "Vscode"],
     reflection:
-      "Dit project was een uitdaging, maar ik heb er veel van geleerd...",
+      "Het onderzoek naar de rol van menselijk brein en AI in websiteontwikkeling onthulde een waardevolle balans tussen creativiteit en technologie. Mijn handgemaakte ontwerp toonde emotionele connectie en unieke signatuur, terwijl AI-tools uitblonken in snelheid en technische precisie. Gebruikerstests benadrukten dat menselijke input essentieel is voor context en esthetiek, terwijl AI efficiëntie en schaalbaarheid biedt. Door de sterke punten van beide benaderingen te combineren, creëerde ik een geoptimaliseerd eindproduct dat zowel functioneel als inspirerend was. Deze ervaring onderstreepte hoe mens en AI elkaar kunnen aanvullen om betere resultaten te bereiken.",
     images: [
-      "/placeholder.svg?height=400&width=600",
-      "/placeholder.svg?height=400&width=600",
-      "/placeholder.svg?height=400&width=600",
+      "/images/Indi_1.svg?height=400&width=600",
+      "/images/Indi_2.svg?height=400&width=600",
+      "/images/Indi_3.svg?height=400&width=600",
     ],
     additionalText:
-      "Hier is wat extra informatie over het project en het proces...",
+      "Mijn proces begon met een diepgaand onderzoek naar beschikbare AI-tools om te begrijpen wat er op de markt is en hoe deze technologieën specifieke taken in design en ontwikkeling kunnen ondersteunen. Dit gaf me de mogelijkheid om bewuste keuzes te maken over welke tools ik zou inzetten en waarvoor. Met deze kennis op zak ging ik aan de slag met het verzamelen van de eisen en wensen van de opdrachtgever, Indi. Dit was een cruciale stap om ervoor te zorgen dat het eindproduct zou aansluiten bij haar verwachtingen en behoeften.",
+    additionalText1:
+      "Daarna begon ik aan een eerste conceptontwerp, volledig vanuit mijn eigen creatieve vermogen. Zonder hulp van AI legde ik mijn ideeën vast om te zien hoe mijn menselijke brein een ontwerp zou aanpakken. Dit werd mijn referentiepunt voor de volgende stap: het inzetten van AI-tools. Ik onderzocht hoe AI kon bijdragen aan zowel het ontwerpen als ontwikkelen van de website en genereerde een nieuw ontwerp met behulp van deze technologieën.",
+    additionalText2:
+      "Met twee ontwerpen in handen—mijn handgemaakte concept en het door AI gegenereerde design—voerde ik gebruikerstests uit om de sterke en zwakke punten van beide methodes te identificeren. Feedback van Indi en andere testers gaf waardevolle inzichten in waar AI tekortschiet of juist uitblinkt. Op basis van deze feedback ging ik aan de slag met itereren en optimaliseren. Ik combineerde de unieke kracht van menselijke creativiteit met de snelheid en precisie van AI om een definitief ontwerp te creëren dat zowel esthetisch aantrekkelijk als functioneel en gebruiksvriendelijk was.",
+    additionalText3:
+      "Deze reis liet me zien hoe mens en AI elkaar kunnen aanvullen, en hoe ik beide kan inzetten om het beste resultaat te bereiken. Het eindproduct weerspiegelt deze samenwerking en biedt een optimale balans tussen technologie en menselijke input.",
   },
 
   {
@@ -191,6 +203,12 @@ export default function ProjectPage({ params }: { params: { id: string } }) {
             Meer over dit project
           </h2>
           <p className="text-[#4E4E4E]">{project.additionalText}</p>
+          <br></br>
+          <p className="text-[#4E4E4E]">{project.additionalText1}</p>
+          <br></br>
+          <p className="text-[#4E4E4E]">{project.additionalText2}</p>
+          <br></br>
+          <p className="text-[#4E4E4E]">{project.additionalText3}</p>
         </section>
       </div>
       <Footer />
